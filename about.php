7311441a675fe46f67c2707ba6acc1099e45dba7
N1troZeuS <77198899+N1troZeuS@users.noreply.github.com>
2021-12-13 11:29:37 +0300

    first upload
    create files of site

---
+++ about.php
@@ -0,0 +1,261 @@
+<!doctype html>
+<head>
+<title>Cypher & Nitrozeus - портфолио</title>
+<meta charset="utf-8">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>О нас</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 История создания сайта
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class="landing-con-panel" style="background: url('/img/graphics/panels/about.jpg') no-repeat center;">
+				<div class='landing-ovr-panel'>
+					<div class='landing-tx1-panel'>
+						<h2>We are RPCS3</h2>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Идея проекта</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Идея родилась после просмотра видеоролика одного блогера милионника на площадке YouTube, где он рассказывал о создании приложений, сайтов и так далее. Именно после этого я 
+									 взялся за шаблон rpcs3, который находится в открытом доступе, за что им огромное спасибо. После кардинальных изменений, когда я сделал все под себя, я опубликовал сайт и 
+									 разместил его код на Git hub, что бы все могли использовать его.  <br>
+									<br>
+									 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/project.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/history.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The History (2011 — Present)</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The project was founded by programmers DH and Hykem in May of 2011. RPCS3 was first hosted on Google Code and was eventually migrated to GitHub later on in its development. The emulator's first successful game boot were primarily composed of smaller homebrew projects and focused hardware tests. <br>
+									<br>
+									 The emulator was later publicly released in June of 2012 and gained substantial attention from both the open-source community and PlayStation enthusiasts alike. Today, RPCS3 is co-developed by two new developers, Nekotekina and kd-11 in tandem with an amazing team of GitHub contributors.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>The People</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 The project is currently maintained and organized by a unique cast of characters, all with meaningful roles to contribute to different aspects of the project. Whether it's working on the emulator directly, coordinating community efforts or even keeping this very website current, everyone plays a role that adds to the bigger picture. <br>
+									<br>
+									 Internal team members aren't the only contributors. A lot of the progress made in making PlayStation 3 emulation possible on PC is thanks to the many developers we've received contributions from in our GitHub community. While many developers do come and go, we do welcome anyone and everyone who is interested in improving the codebase.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/team.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Founders</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 A list of the project founders who are also considered former, non-active developers.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="user-con-grouped">
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/nekotekina.png') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/graphics/flags/russia.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							Nitrozeus</span>
+							<span class="user-con-role user-role-lead">
+							Lead Core Developer </span>
+						</div>
+						<div class='user-con-divider role-lead darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Один из основоположников данного сайта. Большая ему благодарность за время, затраченное на работы и исправления в коде. Занимается Front end разработкой, взял на себя работу по внешнему виду и содержимое контента.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>Desktop</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/cpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>AMD Ryzen 3600X (3.6 Hz, 4.5 with boost)</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/gpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>Radeon RX 570</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/ram.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>8GB DDR4 (3666MHz)</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/os.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>Windows 10 CUSTOM EDT.</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="user-con-grouped">
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/Nitro.png') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/graphics/flags/russia.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							Cypher</span>
+							<span class="user-con-role user-role-lead">
+							Lead Core Developer </span>
+						</div>
+						<div class='user-con-divider role-lead darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Nekotekina joined RPCS3 in late 2013 as a contributor. He is currently the project lead and lead core developer. He is working full-time on RPCS3 thanks to the funding provided by our Patrons. Nekotekina is the single-most active developer. </span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>Desktop</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/cpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>Intel i7-2660 (3.4 Hz)</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/gpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>Radeon RX 580</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/ram.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>16GB DDR3 (4066MHz)</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-specbox">
+									<div class="specbox-ico-part darkmode-invert" style="background: url('/img/icons/list/os.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part darkmode-txt">
+										<span>Linux Mint</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
---
+++ birth.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<head>
+<title>День рождения!</title>
+<meta charset="utf-8">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>День рождения</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Приглашаю Вас на свой пятнадцатый день рождения!
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class="landing-con-panel" style="background: url('/img/graphics/panels/about.jpg') no-repeat center;">
+				<div class='landing-ovr-panel'>
+					<div class='landing-tx1-panel'>
+						<h2>День рождения Марата</h2>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Всем привет</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Приветствую всех на моем сайте! Это Марат. Приглашаю Вас на свой день рождения, который состоится 4 января по адресу Рычинская улица, 1
+									 село Тулугановка. Я думаю, что большинство из Вас прекрасно знают этот адрес.<br>
+									<br>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/bl cake.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/team.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Стоит прийти!</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Надеюсь, Вы сможете прийти. Я буду очень рад видеть всех Вас в такой знаменательный день. Поиграем в настольные игры, посидим за вкусной едой, пообщаемся, 
+									 в общем хорошо, я надеюсь, проведем время. 
+									<br>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Ждем Вас</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 Вот такое получилось небольшое приглашение на сайте, которое я сделал сам. Надеюсь, что это произвело на Вас приятное впечатление. </br>
+							 С наилучшими пожеланиями </br>
+							 Марат
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
