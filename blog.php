0d459c7aa848df78ed1d18fa507340e1cf06a624
N1troZeuS <77198899+N1troZeuS@users.noreply.github.com>
2021-12-29 19:40:05 +0300

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<head>
+<title>Cypher & Nitrozeus - портфолио</title>
+<meta charset="utf-8">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Блог</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class="banner-tx2-title fade-up-onstart">
+				<p>
+					 Блог нашего сайта
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class="landing-con-panel" style="background: url('/img/graphics/panels/quickstart.jpg') no-repeat center;">
+				<div class='landing-ovr-panel'>
+					<div class='landing-tx1-panel'>
+						<h2>Блог</h2>
+					</div>
+				</div>
+			</div>
+			<div class="container-con-block darkmode-block">
+				<div class="anchor-point" id="requirements_desktop">
+				</div>
+				<div class="container-con-wrapper">
+					<div class="container-tx1-block darkmode-txt">
+						<div class='container-emp-block'>
+						</div>
+						<h2>Блог нашего сайта</h2>
+					</div>
+					<div class="container-tx2-block darkmode-txt">
+						<p>
+							 Эта страица будет отведена для создания блога сайта, здесь будут публиковаться обновления на сайте, </br>
+							 новости, интересные по мнению авторов события и так далее. На сайте возможны баги и т.д. При обнаружениии сообщать в Discord сервер.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="spec-con-container">
+
+				<div class="spec-con-max darkmode-txt">
+				<div class="spec-con-title spec-max"><h2>Примите участие в создании сайта</h2></div>
+				<div class="spec-con-desc">
+				<h3>Возможно контент, которым вы можете делиться с нами, будет опубликован</h3></div>
+			<div class="container-con-wrapper">
+				<div class="anchor-point" id="requirements_laptop">
+				</div>
+				<div class="container-tx1-block darkmode-txt">
+					<h2>Discord</h2>
+				</div>
+				<div class="container-tx2-block darkmode-txt">
+					<p>
+						 Есть у нас группа в Discord, будем рады Вас видеть, делитесь своим мнением о сайте, делитесь всем, что интересно Вам и тем, о чем хотели бы поведать миру.
+					</p>
+				</div>
+			</div>
+			<div class="container-con-wrapper">
+				<div class="anchor-point" id="requirements_software">
+				</div>
+				<div class="container-tx1-block darkmode-txt">
+					<h2>Пока здесь пусто</h2>
+				</div>
+				<div class="container-tx2-block darkmode-txt">
+					<p>
+						 The software requirements for running the emulator must be met in order for the software to function at all. The PlayStation 3 system software is required because it is utilized to load system files for the emulator such as the PlayStation 3's proprietary system libraries. Linux and BSD based operating systems do not require the Microsoft Visual C++ 2019 redistributable.<br>
+						<br>
+					</p>
+				</div>
+			<div class="generic-con-button ">
+				<a href='#' download>
+				<div class="generic-btn-button">
+					<div class="generic-ico-button" style="background: url('/img/icons/buttons/patcher-h.png') no-repeat center">
+					</div>
+					<div class="generic-tx1-button">
+						<span>Don`t work<span class="generic-tx2-label">Не работает по тех. причинам</span></span>
+					</div>
+				</div>
+				</a>
+			</div>
+			<div class='list-con-container'>
+				<div class='list-tx1-item darkmode-txt'>
+					 1
+				</div>
+				<div class='list-tx2-description darkmode-txt'>
+					<p>
+						 Пока здесь пусто
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</div>
+
+<?php include 'lib/module/inc-footer.php';?>
+
+</body>
+</html>
