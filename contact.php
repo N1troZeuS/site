bc21d5b3b5d2f66ae21840888d8a97ce85038528
N1troZeuS <77198899+N1troZeuS@users.noreply.github.com>
2021-12-30 12:16:16 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<head>
+<title>Cypher & Nitrozeus - портфолио</title>
+<meta charset="utf-8">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Контакты</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Мы в интернете
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class="landing-con-panel" style="background: url('/img/graphics/panels/about.jpg') no-repeat center;">
+				<div class='landing-ovr-panel'>
+					<div class='landing-tx1-panel'>
+						<h2>ВАЭ!</h2>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Discord</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Наверняка, по мере посещения сайта, Вам изрядно надоел наш сервер Discord, у Вас пропало желание его посещать. Оно и понятно, слишком много мы про него писали, 
+									 но мы постараемся поработать над надоедливосью сервера. Все же, пока он еще в работе и при посещении будет пусто, скоро мы это исправим.
+									 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/ue.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/tube.png') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>YouTube</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Канал на ютубе тоже пустует, над этим так же работаем
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Почта</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									Она существует, ее мы регулярно просматриваем. Все сообщения, предложения и так далее ждем именно туда.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/post.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
